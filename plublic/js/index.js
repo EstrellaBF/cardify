fc55a74fe678ccc9810e72ab49e28fc3e7693e05
Danielapariona <daniela.parionac@gmail.com>
2018-01-29 09:04:14 -0500

    Estructura básica del plugin

--- plublic/js/index.js
+++ plublic/js/index.js
@@ -1,1 +1,15 @@
-
+// Funcion anonima autoejecutable
+(function($) {
+  // jquery le hereda todas sus caracteristicas al nvo objeto
+  $.fn.extend({
+    // nombre del componente
+    plugin: function() {
+      // funcion constructora del componente donde ira toda la programacion
+      function inicializar() {
+        // alert("funciona mi plugin")
+        
+      }
+      return $(this).each(inicializar);
+    }
+  });
+})(jQuery);
