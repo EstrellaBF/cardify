7a22aa59a061f05c6064d5ae69310475f96b4b3f
Danielapariona <daniela.parionac@gmail.com>
2018-01-30 15:01:10 -0500

    Modifica estructura del plugin

---
+++ src/index.js
@@ -0,0 +1,21 @@
+(() => {
+  // heredando características
+  $.fn.extend({
+    // nombre de mi plugin
+    cardify: function(defaultUser) {
+      // crear el objeto aquí
+      let defaultOption = {
+        alt: 'alt'
+      };
+      // Se unirá los valores del defaultOptions y el defaultUser
+      opc = $.extend(defaultOption, defaultUser);
+      // función constructora
+      function init() {
+        // console.log($(this).attr('alt'));
+        $(this).addClass('img-plugin').wrap('<figure></figure>').parent().append(`<figcaption> ${$(this).attr('alt')} </figcaption>`);
+      };
+      // por cada elemento que se invoque inicializar el plugin
+      return $(this).each(init);
+    }
+  });
+})(jQuery);
