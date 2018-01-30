7c53365b5b63dc7c0c0eb97652078b01ffcb9754
EstrellaBF <stern.von.nord>
2018-01-30 14:03:57 -0500

    Agrega funcionalidad

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
-$(document).ready(function() {
-  $('.container-img').plugin();
-});
+(function($) {
+  // heredando características
+  $.fn.extend({
+    // nombre de mi plugin
+    cardify: function(defaultUser) {
+      // crear el objeto aquí
+      var defaultOption = {
+        alt: 'alt'
+      };
+      // Se unirá los valores del defaultOptions y el defaultUser
+      opc = $.extend(defaultOption, defaultUser);
+      var init = () => {
+        $(this).addClass('img-plugin').wrap('<figure></figure>').parent().append('<figcaption>' + $(this).attr('alt') +'</figcaption>');
+      };
+      // por cada elemento que se invoque inicializar el plugin
+      return $(this).each(init);
+    }
+  });
+})(jQuery);
