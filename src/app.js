d184bff02aa726d0bf998f07f372adf294745a81
Danielapariona <daniela.parionac@gmail.com>
2018-01-30 14:23:27 -0500

    Funcionalidad con es6

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
-(function($) {
-  // heredando características
+(() => {
   $.fn.extend({
     // nombre de mi plugin
     cardify: function(defaultUser) {
       // crear el objeto aquí
-      var defaultOption = {
+      let defaultOption = {
         alt: 'alt'
       };
       // Se unirá los valores del defaultOptions y el defaultUser
       opc = $.extend(defaultOption, defaultUser);
-      var init = () => {
-        $(this).addClass('img-plugin').wrap('<figure></figure>').parent().append('<figcaption>' + $(this).attr('alt') +'</figcaption>');
+      let init = () => {
+        $(this).addClass('img-plugin').wrap('<figure></figure>').parent().append(`<figcaption>' ${$(this).attr('alt')}</figcaption>`);
       };
       // por cada elemento que se invoque inicializar el plugin
       return $(this).each(init);
